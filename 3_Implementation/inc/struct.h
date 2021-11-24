0338e2f288f4480ac0715a9d6eac83c076822386
mayuri-salankar <mayurisalankar2799@gmail.com>
2021-11-24 17:50:40 +0530

    update README.md

---
+++ 3_Implementation/inc/struct.h
@@ -0,0 +1,15 @@
+#include<math.h>
+switch(Calc_oprn)
+        {
+            case '+': addition();
+                      break;
+ 
+            case '-': subtraction();
+                      break;
+ 
+            case '*': multiplication();
+                      break;
+ 
+            case '/': division();
+                      break;
+       }
